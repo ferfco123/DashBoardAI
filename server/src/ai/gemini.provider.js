f7f5d5e52fd834c7540970283ce7464c3562399a
ferfco123 <ferfco2000@gmail.com>
2026-01-29 23:40:10 -0300

    changed for .nev api-key in gemini provider

--- server/src/ai/gemini.provider.js
+++ server/src/ai/gemini.provider.js
@@ -3,8 +3,9 @@ import { GoogleGenerativeAI } from "@google/generative-ai"
 
 
 
-// const genAI = new GoogleGenerativeAI(process.env.AI_API_UR)
-const genAI = new GoogleGenerativeAI("AIzaSyColh9NyOJ-jtWIuV9opyOL-__CKq5u8Ms")
+
+const genAI = new GoogleGenerativeAI(process.env.AI_API_UR)
+
 
 const model = genAI.getGenerativeModel({
     model: "gemini-2.5-flash"
